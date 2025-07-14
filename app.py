20569712538c918095e9b5b9e5b11332fa4ed901
Mazalem <matheusoligoms@gmail.com>
2025-07-14 11:39:41 +0000

    app.py criado

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Classificador de Folhas de Videiras"
+    )
+    st.write("# Classifica Folhas de Videiras!")
+
+    #carregar modelo
+        
+
+    #carregar imagem
+
+
+    #classificar imagem
+
+
+    if __name__ == "__main__":
+        main()
